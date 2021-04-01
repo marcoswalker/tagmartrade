31aecf2e8d5b8fc15a124520e4747cecd220bd91
= <=>
2021-03-31 23:04:26 -0300

    dialogMe

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ Hooks.on("renderActorSheet", function (sheet, html, character) {
             $('<a style="margin-left:5px;" class="tradePertence" title="Mandar para amigo" data-actor-id="'+ character.actor._id +'" data-item-id="'+ ht.dataset.itemId +'"><i class="fas fa-handshake"></i></a>').insertAfter(ht);
         }
     }
-    html.find(".tradePertence").click(mandaEvento.bind(this));
+    html.find(".tradePertence").click(mandaPertence.bind(this));
 });
 
 Hooks.on("ready", function () {
@@ -20,38 +20,69 @@ function recebeSocket(tradeData) {
     if (game.user.character !== targetActor) return;
     const actor = game.actors.get(tradeData.currentActor);
     const item = actor.getOwnedItem(tradeData.item);
-    targetActor.createOwnedItem(item);
+    let itemquevai = item;
+    itemquevai.data.data.quant = tradeData.quant;
+    itemquevai._data.data.quant = tradeData.quant;
+    const createdItem = targetActor.createOwnedItem(itemquevai);
 }
 
-function mandaEvento(event) {
+function mandaPertence(event) {
+    if (game.user.character === null || game.user.isGM) return ui.notifications.info("Apenas para jogadores.");
     const currentActor = $(event.currentTarget).data("actorId");
     const itemId = $(event.currentTarget).data("itemId");
     const actor = game.actors.get(currentActor);
     const item = actor.getOwnedItem(itemId);
     const items = duplicate(item);
-    let actorTarget = [];
-    let targetActor = new Actor();
-    for (let token of game.user.targets) {
-        actorTarget.push(token.data.actorId);
-        targetActor = token.actor;
-    }
-    let continua = false;
-    for (let user of game.users) {
-        if (user.character === targetActor && user.active) {
-            continua = true;
-        }
-    }
-    const actorVai = actorTarget[0];
-    if (continua) {
-        const tradeData = {
-            item: items._id,
-            currentActor: currentActor,
-            userId: game.user._id,
-            targetActor: actorVai
-        };
-        game.socket.emit('module.tagmartrade', tradeData);
-        actor.deleteOwnedItem(itemId);
-    } else {
-        ui.notifications.warn("Jogador não disponível!");
-    }
+    const users = game.users;
+    let dialog = new Dialog({
+        title: "Enviar item para jogador",
+        content: "<div><label style='margin-right:10px;' class='mediaeval'>Quantidade:</label><input class='quant' type='number' id='quantPert' style='width:40px;'/><label style='margin-right:10px;margin-left:10px;' class='mediaeval'>Jogador:</label><select id='userSelectx' class='users_names'></select></div>",
+        buttons: {
+            sim : {
+                icon: '<i class="fas fa-check"></i>',
+                label: "Enviar",
+                callback: () => {
+                    let quant = parseInt($('.quant').val());
+                    const user = game.users.get($('.users_names').val());
+                    if (quant > 0 && user !== null) {
+                        if (quant > items.data.quant){
+                            ui.notifications.warn("Você não pode enviar mais itens que você tem!");
+                        } else if (quant == items.data.quant) {
+                            const tradeData = {
+                                item: items._id,
+                                currentActor: currentActor,
+                                targetActor: user.character._id,
+                                quant: quant
+                            };
+                            game.socket.emit('module.tagmartrade', tradeData);
+                            actor.deleteOwnedItem(itemId);
+                        } else {
+                            const tradeData = {
+                                item: items._id,
+                                currentActor: currentActor,
+                                targetActor: user.character._id,
+                                quant: quant
+                            };
+                            game.socket.emit('module.tagmartrade', tradeData);
+                            item.update({'data.quant': items.data.quant - quant});
+                        }
+                    } else if (user === null) {
+                        ui.notifications.warn("Tem que ter outro jogador online!");
+                    } else ui.notifications.warn("Escolha um valor maior que zero!");
+                }
+            },
+            nao : {
+                icon: '<i class="fas fa-times"></i>',
+                label: "Cancelar",
+                callback: () => {}
+            }
+        },
+        default: "nao",
+        render: html => {
+            for (let user of users) {
+                if (user.active && user.character !== null && user !== game.user && !user.isGM) $('.users_names').append("<option value='"+user.data._id+"'>"+user.data.name+"</option>");
+            }
+        },
+    });
+    dialog.render(true);
 }
