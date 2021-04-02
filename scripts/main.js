6e6597d9b5f0b14023e777b146fe75704200e7c1
= <=>
2021-04-02 10:39:37 -0300

    vaiGM

--- scripts/main.js
+++ scripts/main.js
@@ -34,12 +34,12 @@ function recebeSocket(tradeData) {
             actor: game.user.character
         })
     };
-    chatData.content = "<p><img src='"+ actor.img +"' style='float: left; margin-left: auto; margin-right: auto; width: 40%;border: 0px;' /><img src='systems/tagmar_rpg/assets/TAGMAR FOUNDRY.png' style='float: left;margin-top:25px; margin-left: auto; margin-right: auto; width: 20%;border: 0px;'/><img src='"+ targetActor.img +"' style='float: left; width: 40%; margin-left: auto; margin-right: auto;border: 0px;' /></p><p class='rola_desc' style='display: block;margin-left:auto;margin-right:auto;margin-top:50%;'>"+ "<b>" + actor.data.name + "</b> acaba de presentear <b>"+ targetActor.data.name +"</b> com "+ String(tradeData.quant) +" <b>"+ itemquevai.data.name +"</b>." +"</p>";
+    chatData.content = "<p><img src='"+ actor.img +"' style='float: left; margin-left: auto; margin-right: auto; width: 40%;border: 0px;' /><img src='systems/"+ game.system.id +"/assets/TAGMAR FOUNDRY.png' style='float: left;margin-top:25px; margin-left: auto; margin-right: auto; width: 20%;border: 0px;'/><img src='"+ targetActor.img +"' style='float: left; width: 40%; margin-left: auto; margin-right: auto;border: 0px;' /></p><p class='rola_desc' style='display: block;margin-left:auto;margin-right:auto;margin-top:50%;'>"+ "<b>" + actor.data.name + "</b> acaba de presentear <b>"+ targetActor.data.name +"</b> com <b>"+ String(tradeData.quant) +"</b> <b>"+ itemquevai.data.name +"</b>." +"</p>";
     ChatMessage.create(chatData);
 }
 
 function mandaPertence(event) {
-    if (game.user.character === null || game.user.isGM) return ui.notifications.info("Apenas para jogadores.");
+    //if (game.user.character === null || game.user.isGM) return ui.notifications.info("Apenas para jogadores.");
     const currentActor = $(event.currentTarget).data("actorId");
     const itemId = $(event.currentTarget).data("itemId");
     const actor = game.actors.get(currentActor);
