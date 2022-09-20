e21c6981f91eb92a3af987930b224a82dfc3b3c1
= <=>
2022-09-20 11:40:32 -0300

    v10 Foundry

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,8 @@
 Hooks.on("renderActorSheet", function (sheet, html, character) {
-    if (character.actor.type !== "Personagem") return;
+    if (character.document.type !== "Personagem") return;
     for (let ht of html.find(".movePertence")) {
         if ($(ht).attr('title') === "Mover para Transporte") {
-            $('<a style="margin-left:5px;" class="tradePertence" title="Mandar para amigo" data-actor-id="'+ character.actor.id +'" data-item-id="'+ ht.dataset.itemId +'"><i class="fas fa-handshake"></i></a>').insertAfter(ht);
+            $('<a style="margin-left:5px;" class="tradePertence" title="Mandar para amigo" data-actor-id="'+ character.document.id +'" data-item-id="'+ ht.dataset.itemId +'"><i class="fas fa-handshake"></i></a>').insertAfter(ht);
         }
     }
     html.find(".tradePertence").click(mandaPertence.bind(this));
@@ -11,7 +11,7 @@ Hooks.on("renderActorSheet", function (sheet, html, character) {
 Hooks.on("ready", function () {
     if (game.system.id === "tagmar_rpg" || game.system.id === "tagmar") {
         game.socket.on('module.tagmartrade', tradeData => {
-            recebeSocket(tradeData);
+            if (tradeData.type == "trade") recebeSocket(tradeData);
         });
     } else {
         return ui.notifications.error("O módulo Tagmar Transações, só funciona com o sistema Tagmar.");
@@ -24,7 +24,7 @@ function recebeSocket(tradeData) {
     if (game.user.character !== targetActor) return;
     const actor = game.actors.get(tradeData.currentActor);
     let itemquevai = tradeData.itemc;
-    itemquevai.data.quant = tradeData.quant;
+    itemquevai.system.quant = tradeData.quant;
     targetActor.createEmbeddedDocuments("Item", [itemquevai]);//targetActor.createOwnedItem(itemquevai);
     const chatData = {
         user: game.user.id,
@@ -32,9 +32,9 @@ function recebeSocket(tradeData) {
             actor: game.user.character
         })
     };
-    chatData.content = "<p><img src='"+ actor.img +"' style='float: left; margin-left: auto; margin-right: auto; width: 40%;border: 0px;' /><img src='systems/"+ game.system.id +"/assets/TAGMAR FOUNDRY.png' style='float: left;margin-top:25px; margin-left: auto; margin-right: auto; width: 20%;border: 0px;'/><img src='"+ targetActor.img +"' style='float: left; width: 40%; margin-left: auto; margin-right: auto;border: 0px;' /></p><p class='rola_desc' style='display: block;margin-left:auto;margin-right:auto;margin-top:60%;'>"+ "<b>" + actor.data.name + "</b> acaba de presentear <b>"+ targetActor.data.name +"</b> com <b>"+ String(tradeData.quant) +"</b> <b>"+ itemquevai.name +"</b>." +"</p>";
+    chatData.content = "<p class='mediaeval'><img src='"+ actor.img +"' style='float: left; margin-left: auto; margin-right: auto; width: 40%;border: 0px;' /><img src='systems/"+ game.system.id +"/assets/TAGMAR FOUNDRY.png' style='float: left;margin-top:25px; margin-left: auto; margin-right: auto; width: 20%;border: 0px;'/><img src='"+ targetActor.img +"' style='float: left; width: 40%; margin-left: auto; margin-right: auto;border: 0px;' /></p><p class='rola_desc' style='display: block;margin-left:auto;margin-right:auto;margin-top:60%;'>"+ "<b>" + actor.name + "</b> acaba de presentear <b>"+ targetActor.name +"</b> com <b>"+ String(tradeData.quant) +"</b> <b>"+ itemquevai.name +"</b>." +"</p>";
     ChatMessage.create(chatData);
-    ui.notifications.info("Você acaba de receber " + tradeData.quant + " " + itemquevai.name + " de " + actor.data.name);
+    ui.notifications.info("Você acaba de receber " + tradeData.quant + " " + itemquevai.name + " de " + actor.name);
 }
 
 function mandaPertence(event) {
@@ -42,7 +42,7 @@ function mandaPertence(event) {
     const currentActor = $(event.currentTarget).data("actorId");
     const itemId = $(event.currentTarget).data("itemId");
     const actor = game.actors.get(currentActor);
-    const item =  actor.data.items.get(itemId); //actor.getOwnedItem(itemId);
+    const item =  actor.items.get(itemId); //actor.getOwnedItem(itemId);
     const items = item.clone();
     const users = game.users;
     let dialog = new Dialog({
@@ -56,26 +56,28 @@ function mandaPertence(event) {
                     let quant = parseInt($('.quant').val());
                     const user = game.users.get($('.users_names').val());
                     if (quant > 0 && user !== null) {
-                        if (quant > item.data.quant){
+                        if (quant > item.system.quant){
                             ui.notifications.warn("Você não pode enviar mais itens que você tem!");
-                        } else if (quant == items.data.data.quant) {
+                        } else if (quant == items.system.quant) {
                             const tradeData = {
-                                itemc: items.data,
+                                itemc: items,
                                 currentActor: currentActor,
                                 targetActor: user.character.id,
-                                quant: quant
+                                quant: quant,
+                                type: "trade"
                             };
                             game.socket.emit('module.tagmartrade', tradeData);
                             actor.deleteEmbeddedDocuments("Item", [itemId]);//actor.deleteOwnedItem(itemId);
                         } else {
                             const tradeData = {
-                                itemc: items.data,
+                                itemc: items,
                                 currentActor: currentActor,
                                 targetActor: user.character.id,
-                                quant: quant
+                                quant: quant,
+                                type: "trade"
                             };
                             game.socket.emit('module.tagmartrade', tradeData);
-                            actor.updateEmbeddedDocuments("Item", [{'_id': item.id, 'data.quant': items.data.data.quant - quant}])
+                            actor.updateEmbeddedDocuments("Item", [{'_id': item.id, 'data.quant': items.system.quant - quant}])
                         }
                     } else if (user === null) {
                         ui.notifications.warn("Tem que ter outro jogador online!");
@@ -91,7 +93,7 @@ function mandaPertence(event) {
         default: "nao",
         render: html => {
             for (let user of users) {
-                if (user.active && user.character !== null && user !== game.user && !user.isGM) $('.users_names').append("<option value='"+user.id+"'>"+user.data.name+"</option>");
+                if (user.active && user.character !== null && user !== game.user && !user.isGM) $('.users_names').append("<option value='"+user.id+"'>"+user.name+"</option>");
             }
         },
     });
---
+++ scripts/vote.js
@@ -0,0 +1,146 @@
+let votacoes = {
+    escolhas: [],
+    titulos: [],
+    votes: []
+};
+
+Hooks.on("getSceneControlButtons", (controls) => {
+    const bar = controls.find(c => c.name === "token");
+    bar.tools.push({
+      name: "Iniciar Votação",
+      icon: "fas fa-vote-yea",
+      title: "Iniciar Votação para todos jogadores.",
+      onClick: async () => set_votation(),
+      button: true
+    });
+});
+
+function set_votation(event) {
+    let titulos = [];
+    votacoes = {
+        escolhas: [],
+        titulos: [],
+        votes: []
+    };
+    const dialog_html = `
+        <label for="titulo">Opção: </label>
+        <input type="text" id="titulo" style="width: 250px;"/>
+        <input type="button" id="adicionar" value="Adicionar"/>
+        <ul id="lista" style="height: 400px; list-style-type: none;"></ul>
+    `;
+    let dialog = new Dialog({
+        title: "Votação",
+        content: dialog_html,
+        buttons: {
+            "enviar": {
+                label: "Enviar",
+                icon: '<i class="fas fa-check"></i>',
+                callback: html => {
+                    let checkeds = $("input[type='checkbox']:checked");
+                    let escolha = checkeds.length > 0 ? checkeds[0].value: false;
+                    const socketData = {
+                        type: "vote",
+                        user: game.user.name,
+                        titulos: titulos,
+                        escolha: escolha
+                    };
+                    votacoes.escolhas.push({user: game.user.name, escolha: escolha});
+                    votacoes.titulos = titulos;
+                    game.socket.emit('module.tagmartrade', socketData);
+                }
+            }
+        },
+        render: html => {
+            let titulo = $(html.find("#titulo"));
+            let lista = $(html.find("#lista"));
+            let addOption = function () {
+                lista.append(`<li><input class="check" type="checkbox" value="${titulos.length}"/>${titulo.val()}</li>`);
+                titulos.push(titulo.val());
+                titulo.val("");
+                $(html.find('.check')).change(function (event) {
+                    $(html.find('.check')).not(event.currentTarget).prop('checked', false);
+                });
+            }
+            $(html.find("#adicionar")).click(addOption);
+            $(html.find("#titulo")).on("keypress", function (event) {
+                if (event.which === 13) addOption();
+            }); 
+        }
+    }).render(true);
+}
+
+Hooks.on("ready", function () {
+    game.socket.on("module.tagmartrade", socketData => {
+       if (socketData.type == "vote") recebeSocket(socketData);
+       else if (socketData.type == "vote_send") recebeVote(socketData);
+    });
+});
+
+function recebeVote(socketData) {
+    if (game.user.name != socketData.user_ini) return;
+    votacoes.escolhas.push({user: socketData.user, escolha: socketData.escolha});
+    if (votacoes.escolhas.length >= game.users.filter(u => u.active).length) {
+        // todos os votos computados
+        for (let titulo_id = 0; titulo_id < votacoes.titulos.length; titulo_id++) {
+            votacoes.votes[titulo_id] = 0;
+            for (let escolha of votacoes.escolhas) {
+                if (parseInt(escolha.escolha) == titulo_id) votacoes.votes[titulo_id] += 1;  
+            }    
+        }
+        let html_chat = "";
+        let colors = ["red", "purple", "lime", "teal", "aqua", "chocolate", "darkcyan", "darkgoldenrod", "darkkhaki", "darksalmon", "deeppink", "dodgerblue"];
+        let soma_total = 0;
+        for (let vote of votacoes.votes) {
+            soma_total += vote;
+        }
+        for (let title of votacoes.titulos) {
+            html_chat += `<p>${title}</p>
+            <div style="width: 100%; height: 37px; background-color: #ddd;">
+              <div style="text-align: right; padding-top: 10px; padding-bottom: 10px; color: white; height: 100%; width: ${(votacoes.votes[votacoes.titulos.indexOf(title)] / soma_total) * 100}%; background-color: ${colors[votacoes.titulos.indexOf(title)]};">${((votacoes.votes[votacoes.titulos.indexOf(title)] / soma_total) * 100).toFixed(1).replace(/\.0+$/,"")}%</div>
+            </div>`;
+        }        
+        ChatMessage.create({
+            user: game.user.id,
+            content: html_chat
+        });
+        votacoes = {
+            escolhas: [],
+            titulos: [],
+            votes: []
+        };
+    }
+}
+
+function recebeSocket(socketData) {
+    let dialog_html = `<ul style="list-style-type: none; height: 400px;" id="lista"></ul>`;
+    let dialog = new Dialog({
+        title: `Votação iniciada por ${socketData.user}`,
+        content: dialog_html,
+        buttons: {
+            "enviar": {
+                label: "Enviar",
+                icon: '<i class="fas fa-check"></i>',
+                callback: html => {}
+            }
+        },
+        render: html => {
+            for (let titulo of socketData.titulos) {
+                $(html.find("#lista")).append(`<li><input type="checkbox" class="check" value="${socketData.titulos.indexOf(titulo)}">${titulo}</li>`);
+            }
+            $(html.find('.check')).change(function (event) {
+                $(html.find('.check')).not(event.currentTarget).prop('checked', false);
+            });
+        },
+        close: html => {
+            let checkeds = $("input[type='checkbox']:checked");
+            let escolha = checkeds.length > 0 ? checkeds[0].value : false;
+            let send_vote = {
+                type: "vote_send",
+                user: game.user.name,
+                user_ini: socketData.user,
+                escolha: escolha
+            };
+            game.socket.emit("module.tagmartrade", send_vote);
+        }
+    }).render(true);
+}
